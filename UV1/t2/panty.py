a0ac9b4b063a7a3347a2966d3f7d5f13f6e7c94e
liqowomo <123171512+liqowomo@users.noreply.github.com>
2024-09-30 18:34:00 +0000

    🫦

--- UV1/t2/panty.py
+++ UV1/t2/panty.py
@@ -1,5 +1,13 @@
+from rich.traceback import install
+from src.utils.ban import banner
+from src.app.p1 import func_p1
+
+install(show_locals=True)
+
+
 def main():
-    print("Hello from t2!")
+    banner("UV test")
+    func_p1()
 
 
 if __name__ == "__main__":
