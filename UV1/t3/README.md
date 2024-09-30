f0a13d8a155797032d54c69229d1ae786b757108
liqowomo <123171512+liqowomo@users.noreply.github.com>
2024-09-30 18:52:22 +0000

    🫦

---
+++ UV1/t3/README.md
@@ -0,0 +1,4 @@
+# T3 
+
+Simple textual rich based program for testing purposes. And using wiht UV 
+
---
+++ UV1/t3/panty.py
@@ -0,0 +1,12 @@
+from src.p1 import *
+from rich.traceback import install
+
+install(show_locals=True)
+
+
+def main():
+    banner("Smell here as and pussy now")
+
+
+if __name__ == "__main__":
+    main()
